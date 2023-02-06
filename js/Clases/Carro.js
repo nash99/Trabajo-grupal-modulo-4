87e3f2776434245619c6daceb6c35b3d52afcc6c
Juan Salas <juan.salas.salas@outlook.com>
2023-02-06 20:29:17 -0300

    funcion agregar al carro

--- js/Clases/Carro.js
+++ js/Clases/Carro.js
@@ -1,14 +1,11 @@
 
 class Carro{
-
     productos = [];
     neto;
     bruto;
     descuento;
     total;
     iva; 
-
-    
 
     //Getters
     get productos(){return this.productos};
@@ -26,6 +23,8 @@ class Carro{
     set descuento(_descuento){this.descuento = _descuento};
     set total(_total){this.total = _total};
     set iva(_iva){ this.iva = _iva}
+    //Constructor
+    
     
     //Custom
     calcularTotales = () =>{
@@ -40,10 +39,21 @@ class Carro{
             }
             this.total = this.neto - this.descuento
         });
+        return this.neto,this.iva,this.bruto,this.descuento,this.total
     }
 
-    añadirProducto = (producto) =>{
-        this.productos.push(producto)
+    añadirProducto = (productoNuevo) =>{
+        if(carrito.productos.find(producto => producto.codigo == productoNuevo.codigo)){
+            let prod = carrito.productos.find(producto => producto.codigo == productoNuevo.codigo)
+            prod.cantidad += 1
+            carrito.productos.indexOf(prod)
+            console.log(prod)
+        }else{
+            productoNuevo.cantidad += 1
+            this.productos.push(productoNuevo)
+            console.log(productoNuevo)
+        }
+        
     }
 
     eliminarCarro = () =>{
@@ -54,6 +64,8 @@ class Carro{
         this.descuento = 0;
         this.bruto = 0;
     }
+
+    
    
 
 }
---
+++ js/main.js
@@ -0,0 +1,50 @@
+
+// Variables y eventos
+const cards = document.querySelector('.cards') 
+const tabla = document.querySelector('#tabla') 
+const contenidoTabla = document.querySelector('#contenido-tabla') 
+const vaciarCarrito = document.querySelector('#vaciar-carrito')
+const pagarCarrito = document.querySelector('#pagar-carrito')
+const totalNeto = document.querySelector('#totalNeto')
+const totalIva = document.querySelector('#totalIva')
+const netoIva = document.querySelector('#netoIva')
+const precioEnvio = document.querySelector('#precioEnvio')
+const totalTotal = document.querySelector('#totalTotal')
+const contenedorTotales = document.querySelector('#contenedor-totales')
+const botones = document.querySelector('.botones')
+let carrito = new Carro();
+
+
+window.addEventListener('DOMContentLoaded', mostrarProductos)
+
+
+/**
+ * Muestra en el DOM todos los productos del arreglo
+ */
+function mostrarProductos() {
+    i = 1
+    catalogo.forEach(producto => { 
+        
+        const card = document.createElement('div')
+        card.classList.add('card', 'p-2')
+        card.innerHTML = 
+        `
+        <img id="img-${i}" class="w-100 mb-2" src="${producto.imagen}">
+        <p id="nombre-${i}">${producto.nombre}</p>
+        <p id="codigo-${i}">Código: ${producto.codigo}</p>
+        <p id="descripcion-${i}">Descripción: ${producto.descripcion}.</p>
+        <p id="precio-${i}">Precio:$${producto.precio}</p>
+        <p id="stock-${i}">Stock: ${producto.stock}</p>
+        <button id="boton-${i}" type="button" class="btn bg-primary agregarProducto">Agregar al carrito</button>
+        `
+        i++
+        cards.appendChild(card)
+    })
+}
+
+//Agregar al carro
+$(document).on('click','button[type="button"]', function() {
+    let id = parseInt(this.id[this.id.length - 1]) ;
+    let producto = catalogo.find(producto => producto.codigo == id)
+    carrito.añadirProducto(producto)
+  });
